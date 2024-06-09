3603afb182f9a9c699488837d9891cc06f55c58d
Ievhenii <youn4ik@gmail.com>
2024-06-10 01:04:03 +0300

    added apis to registration and login and added axis

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,19 +2,103 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const cors = require('cors');
-
+const passport = require('passport');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const initializePassport = require('./passport-config');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.use(cors());
+
+initializePassport(passport);
+
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const pool = new Pool({
-  user: 'user', //  your PostgreSQL user
-  host: 'db', // the name of the PostgreSQL service in the docker-compose.yml file
-  database: 'postgres_db', // the name of your database
-  password: 'j49OPekq9PlKj', // password to the database
-  port: 5432, // port from docker-compose.yml
+  user: 'user',
+  host: 'db',
+  database: 'postgres_db',
+  password: 'j49OPekq9PlKj',
+  port: 5432,
+});
+
+// JWT secret
+const JWT_SECRET = 'your_jwt_secret';
+
+// Middleware for checking JWT
+function authenticateJWT(req, res, next) {
+  const token = req.cookies.token;
+
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+// CORS configuration
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.post('/register', async (req, res) => {
+  try {
+    const { name, surname, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const { rows } = await pool.query(
+      'INSERT INTO users (name, surname, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
+      [name, surname, email, hashedPassword]
+    );
+    res.json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(eirr.message);
+  }
+});
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(400).json({ message: info.message });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      // Generate JWT
+      const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
+      // Set cookie
+      res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
+      return res.json({ message: 'Logged in successfully', user });
+    });
+  })(req, res, next);
+});
+
+app.get('/protected', authenticateJWT, (req, res) => {
+  res.json({ message: 'This is a protected route', user: req.user });
 });
 
 app.get('/', async (req, res) => {
@@ -26,7 +110,6 @@ app.get('/', async (req, res) => {
   }
 });
 
-// Endpoint to retrieve all users
 app.get('/users', async (req, res) => {
   try {
     const { rows } = await pool.query('SELECT * FROM users');
@@ -39,7 +122,6 @@ app.get('/users', async (req, res) => {
 
 app.post('/users', async (req, res) => {
   try {
-    console.log('Creating a new use...', req.body);
     const { name, surname, email } = req.body;
     const { rows } = await pool.query(
       'INSERT INTO users (name, surname, email) VALUES ($1, $2, $3) RETURNING *',
@@ -51,7 +133,6 @@ app.post('/users', async (req, res) => {
     res.status(500).send(err.message);
   }
 });
-
 
 app.delete('/users/:id', async (req, res) => {
   try {
---
+++ packages/web/src/app/protected/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from 'next/navigation';
+import styles from "./protected.module.css";
+
+interface User {
+  id: number;
+  email: string;
+  name: string;
+  surname: string;
+}
+
+const Protected: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchProtectedData = async () => {
+      try {
+        const response = await axios.get<{ user: User }>('http://localhost:8080/protected', { withCredentials: true });
+        setUser(response.data.user);
+      } catch (error) {
+        // @ts-ignore
+        setError(error.response ? error.response.data.message : error.message);
+        router.push('/login');
+      }
+    };
+
+    fetchProtectedData();
+  }, [router]);
+
+  return (
+    <article className={styles.root}>
+      <h2>Protected</h2>
+      {user ? (
+        <div>
+          <h2>Welcome, {user.email}</h2>
+          <h2>Get and Delete Users</h2>
+
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+      {error && <div>Error: {error}</div>}
+      <div className={styles.container}></div>
+      <div style={{ height: 900 }}></div>
+    </article>
+  );
+};
+
+export default Protected;
